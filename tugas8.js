6c8337a9e9cc8ad5b063d8bf42e315ca1ea8cd71
zainahda <zainahda@gmail.com>
2022-02-02 15:39:24 +0700

    Niomic tugas 8 Js

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function tugasObject() {
+  let dataKaryawan = {
+    nama: "Zain",
+    umur: 32,
+    gender: "Pria",
+    pengalaman: "5 Tahun"
+  }
+  dataKaryawan.pengalaman = "7 Tahun";
+  console.log(dataKaryawan);
+}
+
+tugasObject();
